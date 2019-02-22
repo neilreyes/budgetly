842bcab1cee0f2127de14a302f91f49fc80a23b7
Neil Reyes <rayneilreyes@gmail.com>
2019-02-23 00:39:16 +0800

    - Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../assets/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Budgetly
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
