9e9c1c3506d7a8d604d625936b0e18b4d973fbf4
Neil Reyes <rayneilreyes@gmail.com>
2019-02-23 16:43:51 +0800

    Add list item of transaction entry

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './SiteHeader';
 import '../assets/styles/App.css';
 import SiteHeader from './SiteHeader';
+import SiteBody from './SiteBody';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SiteHeader />
+        <SiteBody />
       </div>
     );
   }
---
+++ src/components/SiteBody.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+    Grid,
+    List,
+    ListItem,
+    ListItemText,
+    withTheme,
+    withStyles,
+} from '@material-ui/core';
+
+const styles = theme =>({
+    root: {
+        flexGrow: 1,
+        margin: '30px auto',
+        padding: '0 20px',
+        [theme.breakpoints.up("lg")]: {
+            width: 1170
+        }
+    },
+    amountMeta: {
+        textAlign: 'right',
+    }
+});
+
+const SiteBody = (props) => {
+    const { classes } = props;
+    return(
+        <div className={classes.root}>
+            <Grid
+                justify="center"
+                container
+                spacing={6}
+            >
+                <Grid
+                    alignItems="center"
+                    container
+                    className={classes.container}
+                    justify="center"
+                >
+                    <Grid item xs={12}>
+                        <List>
+                            <ListItem
+                                button
+                            >
+                                <ListItemText
+                                    primary="Title"
+                                    secondary="Category | Account (e.g Wallet,Paypal)"
+                                />
+                                <ListItemText
+                                    className={classes.amountMeta}
+                                    primary="Amount"
+                                    secondary="Paid or Received"
+                                />
+                         
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default withStyles(styles)(SiteBody);
