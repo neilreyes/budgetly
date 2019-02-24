290cfe1f5d59fa6356f0018a485780deacbb93cf
Neil Reyes <rayneilreyes@gmail.com>
2019-02-24 23:50:55 +0800

    Add footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import './SiteHeader';
-import '../assets/styles/App.css';
+// Components
 import SiteHeader from './SiteHeader';
 import SiteBody from './SiteBody';
+import SiteFooter from './SiteFooter';
+// Material
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+ palette:{
+   primary: { main: '#2ca4ff' },
+ }
+});
 
 class App extends Component {
+  state = {
+    isDialogBoxOpen: false,
+  }
+
   render() {
     return (
-      <div className="App">
-        <SiteHeader />
-        <SiteBody />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <SiteHeader />
+          <SiteBody />
+          <SiteFooter />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
