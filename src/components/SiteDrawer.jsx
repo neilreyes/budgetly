57f3536ec7ece2f545038c95542a9abe96fa032d
Neil Reyes <rayneilreyes@gmail.com>
2019-02-26 12:09:44 +0800

    Implement toggle drawer to site header app bar and site drawer

--- src/components/SiteDrawer.jsx
+++ src/components/SiteDrawer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { classNames } from 'classnames';
 import {
     Drawer,
     IconButton,
+    Toolbar,
+    withStyles,
     withTheme } from '@material-ui/core';
-import { connect } from 'react-redux';
 import {
     CreditCard,
     TrendingDown,
@@ -12,25 +15,59 @@ import {
     ChevronLeft,
     ChevronRight } from '@material-ui/icons';
 
+const drawerWidth = 240;
+
+const styles = theme => (
+    {
+        drawer: {
+            width: drawerWidth,
+        },
+        drawerOpen: {
+            width: drawerWidth,
+        },
+        drawerClose: {
+            width: 66,
+        },
+        toolBar: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            padding: "0 8px",
+            ...theme.mixins.toolbar,
+        }
+    }
+)
+
 class SiteDrawer extends React.Component{
+    
     render(){
+        const { classes } = this.props;
+        console.log(this.props);
         return(
             <Drawer
                 anchor="left"
+                className={ this.props.open ? classes.drawerOpen : classes.drawerClose }
+                classes={{
+                    paper: this.props.open ? classes.drawerOpen : classes.drawerClose
+                }}
                 open={this.props.open}
                 variant="permanent">
-                <IconButton
-                    onClick={ ()=>{this.props.toggleDrawer();} }>
-                     { this.props.open ? <ChevronLeft/> : <ChevronRight/> }
-                </IconButton>
+                <div
+                    className={classes.toolBar}>
+                    <IconButton
+                        onClick={ ()=>{this.props.toggleDrawer();} }>
+                        { this.props.open ? <ChevronLeft/> : <ChevronRight/> }
+                    </IconButton>
+                </div>
             </Drawer>
         )
     };
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
     return {
-        open: state.siteDrawer.open
+        open: state.siteDrawer.open,
+        classes: ownProps.classes,
     };
 }
 
@@ -45,5 +82,6 @@ export default compose(
         mapStateToProps, 
         mapDispatchToProps,
     ),
-    withTheme()
+    withTheme(),
+    withStyles(styles),
 )(SiteDrawer);
--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,32 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { 
-    AppBar, 
-    Toolbar, 
-    Typography, 
-    withStyles,
-    Button
-} from '@material-ui/core';
+    AppBar,
+    Button,
+    IconButton,
+    Toolbar,    
+    Typography,
+    withStyles, 
+    withTheme } from '@material-ui/core';
+import { Menu, Close } from '@material-ui/icons';
 
-const styles = {
+const drawerWidth = 240;
+
+const styles = theme => ({
     root: {
         justifyContent: 'space-between',
+    },
+    appBar: {
+        marginLeft: 66,
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    appBarShift: {
+        marginLeft: 240,
+        width: `calc(100% - ${drawerWidth}px)`,
+    }
+});
+
+class SiteHeader extends Component {
+
+
+    render(){
+        const { classes, isDrawerOpen, toggleDrawer } = this.props;
+
+        return (
+            <AppBar
+                className={ isDrawerOpen ? classes.appBarShift : classes.appBar }
+                position="static">
+                <Toolbar>
+                    <IconButton onClick={()=>toggleDrawer()}>
+                        { isDrawerOpen ? <Close /> : <Menu /> }
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Budgetly
+                    </Typography>
+                    <Button color="inherit">
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        );
     }
 }
-const SiteHeader = (props) => {
-    const {classes} = props;
 
-    return (
-        <AppBar position="static" >
-            <Toolbar className={classes.root}>
-                <Typography variant="h6" color="inherit">
-                    Budgetly
-                </Typography>
-                <Button color="inherit">
-                    Login
-                </Button>
-            </Toolbar>
-        </AppBar>
-    )
+const mapStateToProps = (state, ownProps) =>{
+    return {
+        isDrawerOpen : state.siteDrawer.open,
+        classes: ownProps.classes,
+    }
 }
 
-export default withStyles(styles)(SiteHeader);
+const mapDispatchToProps = dispatch =>{
+    return {
+        toggleDrawer : () => dispatch({type: "TOGGLE_DRAWER"}),
+    }
+}
+
+export default compose(
+    withTheme(),
+    withStyles(styles, { withTheme: true }),
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    )
+)(SiteHeader);
