438de1a9c64198b78dbd6e5fa8bfb9a4663f67dd
RR-D <rayneilreyes@gmail.com>
2019-02-26 13:19:32 +0800

    Merge App component from remote

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import '../assets/styles/App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        test
-      </div>
-=======
 // Components
 import SiteDrawer from './SiteDrawer';
 import SiteHeader from './SiteHeader';
 import SiteBody from './SiteBody';
-//import SiteFooter from './SiteFooter';
+// import SiteFooter from './SiteFooter';
 // Material
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 
@@ -42,7 +32,6 @@ class App extends Component {
           {/* <SiteFooter /> */}
         </div>
       </MuiThemeProvider>
->>>>>>> 834aac4b87ef649fd56927d4f8c3109bd8fa9eab
     );
   }
 }
